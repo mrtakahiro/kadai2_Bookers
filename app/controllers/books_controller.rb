fabd880c2f88ba2489cf19c03b07873e4143a1b8
mrtakahiro <128272534+mrtakahiro@users.noreply.github.com>
2023-04-19 12:38:04 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,10 +21,16 @@ class BooksController < ApplicationController
     redirect_to book_path(@book)
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+  end
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(book.id),　flash: { notice: '作成されました' }
   end
 
   private
